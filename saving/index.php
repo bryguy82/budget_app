765b6d5e5a18192b41304391aeed44893f5a147d
Anderson-Bryan <43304311+bryguy82@users.noreply.github.com>
2020-02-25 18:24:19 -0300

    Save entries done

--- saving/index.php
+++ saving/index.php
@@ -44,12 +44,10 @@ switch ($action){
             exit;
         }
 
+        // Insert data into the database
         $insertResult = createSaveTracker($name, $type, $category, $rate, $term, $futureValue, $userId);
 
-        // $save = getSaveTrackers($userId, $type);
-
         if ($insertResult === 1) {
-            // $saveTrackersData = buildSaveTrackers($save);
             $message = "<p>Congratulations, your saving tracker was successfully added.</p>";
             include "../view/manage_save.php";
             exit;
@@ -62,10 +60,103 @@ switch ($action){
 
     case 'ViewSave':
 
+        $trackerId = filter_input(INPUT_GET, 'trackerId', FILTER_SANITIZE_NUMBER_INT);
+
+        // Get tracker data
+        $trackerData = getSaveByTrackerId($trackerId);
+        $trackerSource = getTrackerSourceByTrackerId($trackerId);
+        $trackerName = $trackerSource['trackerName'];
+
+        if(!count($trackerData)){
+            $message = "<p class='notice'>Sorry, no tracker data could be found.</p>";
+        } else {
+            // build display for data
+            $trackerDisplay = buildSaveData($trackerData);
+        }
+
         // Data to be viewed on this page
         include '../view/view_save.php';
         break;
 
+    case 'SaveNewEntry':
+
+        $trackerId = filter_input(INPUT_POST, 'trackerId', FILTER_SANITIZE_NUMBER_INT);
+        $saveDate = filter_input(INPUT_POST, 'saveDate', FILTER_SANITIZE_STRING);
+        $start = filter_input(INPUT_POST, 'start', FILTER_SANITIZE_NUMBER_INT);
+        $deposit = filter_input(INPUT_POST, 'deposit', FILTER_SANITIZE_NUMBER_INT);
+
+        if (empty($saveDate) || empty($start) || empty($deposit)) {
+            $message = "<p>All input data must be completed.  Please try again.</p>";
+            include "../view/view_save.php";
+            exit;
+        }
+
+        // Convert date for database
+        $d=strtotime($saveDate);
+        $saveDate = date("Y-m-d h:i:s", $d);
+
+        // Get tracker startup data from tracker table (term, rate)
+        $trackerSource = getTrackerSourceByTrackerId($trackerId);
+        // $trackerSource['term']
+        // $trackerSource['goal']
+
+        // Function to calculate earned and total go here
+        list($earned, $total) = calculateSaveEntry($start, $deposit, $trackerSource['interest']);
+
+        // Function to insert into DB here
+        $insertResult = insertSaveTracker($saveDate, $start, $deposit, $earned, $total, $trackerId);
+
+        if ($insertResult === 1) {
+            $message = "<p>Congratulations, your entry was successfully added.</p>";
+            header("Location: /budget/saving/?action=ViewSave&trackerId=".urldecode($trackerId));
+            // include "../view/manage_save.php";
+            exit;
+        } else {
+            $message = "<p>Sorry, adding your tracker failed.  Please try again.</p>";
+            include "../view/view_save.php";
+            exit;
+        }
+        break;
+
+    case 'SaveEntry':
+
+        $trackerId = filter_input(INPUT_POST, 'trackerId', FILTER_SANITIZE_NUMBER_INT);
+        $saveDate = filter_input(INPUT_POST, 'saveDate', FILTER_SANITIZE_STRING);
+        $deposit = filter_input(INPUT_POST, 'deposit', FILTER_SANITIZE_NUMBER_INT);
+
+        if (empty($saveDate) || empty($deposit)) {
+            $message = "<p>All input data must be completed.  Please try again.</p>";
+            include "../view/view_save.php";
+            exit;
+        }
+
+        // Convert date for database
+        $d=strtotime($saveDate);
+        $saveDate = date("Y-m-d h:i:s", $d);
+
+        // Get tracker startup data from tracker table (term, rate)
+        $trackerSource = getTrackerSourceByTrackerId($trackerId);
+        // $trackerSource['term']
+        // $trackerSource['goal']
+        $startValue = getMaxStart($trackerId);
+
+        // Function to calculate earned and total go here
+        list($earned, $total) = calculateSaveEntry($startValue['total'], $deposit, $trackerSource['interest']);
+
+        // Function to insert into DB here
+        $insertResult = insertSaveTracker($saveDate, $startValue['total'], $deposit, $earned, $total, $trackerId);
+
+        if ($insertResult === 1) {
+            $message = "<p>Congratulations, your entry was successfully added.</p>";
+            header("Location: /budget/saving/?action=ViewSave&trackerId=".urldecode($trackerId));
+            // include "../view/manage_save.php";
+            exit;
+        } else {
+            $message = "<p>Sorry, adding your tracker failed.  Please try again.</p>";
+            include "../view/view_save.php";
+            exit;
+        }
+        break;
 
     default:
         include '../view/home.php';
