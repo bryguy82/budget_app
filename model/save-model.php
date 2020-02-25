84154b04c458d6e438271046053b1c67aef1af43
Anderson-Bryan <43304311+bryguy82@users.noreply.github.com>
2020-02-25 18:23:02 -0300

    database functions added

--- model/save-model.php
+++ model/save-model.php
@@ -44,9 +44,74 @@ function getSaveTrackers($userId, $type) {
     return $userData;
 }
 
+// Get tracker saving table
+function getSaveByTrackerId($trackerId) {
 
+    $db = dbConnect();
+    $sql = 'SELECT * FROM save WHERE trackerId = :trackerId';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':trackerId', $trackerId, PDO::PARAM_INT);
+    $stmt->execute();
+    $trackerData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt->closeCursor();
+    return $trackerData;
+}
 
+// Get tracker main data
+function getTrackerSourceByTrackerId($trackerId) {
 
+    $db = dbConnect();
+    $sql = 'SELECT * FROM trackers WHERE trackerId = :trackerId';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':trackerId', $trackerId, PDO::PARAM_INT);
+    $stmt->execute();
+    $trackerData = $stmt->fetch(PDO::FETCH_ASSOC);
+    $stmt->closeCursor();
+    return $trackerData;
+}
 
+// Insert new row to saving table
+function insertSaveTracker($saveDate, $start, $deposit, $earned, $total, $trackerId) {
+
+    $db = dbConnect();
+
+    $sql = 'INSERT INTO save (saveDate, start, deposit, interestEarned, total, trackerId)
+            VALUES (:saveDate, :start, :deposit, :earned, :total, :trackerId)';
+    
+    // Prepare statement using the db connection
+    $stmt = $db->prepare($sql);
+
+    // Variable setters
+    $stmt->bindValue(':saveDate', $saveDate, PDO::PARAM_STR);
+    $stmt->bindValue(':start', $start, PDO::PARAM_INT);
+    $stmt->bindValue(':deposit', $deposit, PDO::PARAM_INT);
+    $stmt->bindValue(':earned', $earned, PDO::PARAM_INT);
+    $stmt->bindValue(':total', $total, PDO::PARAM_INT);
+    $stmt->bindValue(':trackerId', $trackerId, PDO::PARAM_INT);
+
+    // Perform the insert with the data
+    $stmt->execute();
+    // Verify success with a row-changed count
+    $rowsChanged = $stmt->rowCount();
+    // Close the connection
+    $stmt->closeCursor();
+    // Return the info that verifies success
+    return $rowsChanged;
+}
+
+/**
+ * Get max total value from tracker
+ */
+function getMaxStart($trackerId) {
+
+    $db = dbConnect();
+    $sql = 'SELECT MAX(total) AS total FROM save WHERE trackerId = :trackerId';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':trackerId', $trackerId, PDO::PARAM_INT);
+    $stmt->execute();
+    $trackerData = $stmt->fetch(PDO::FETCH_ASSOC);
+    $stmt->closeCursor();
+    return $trackerData;
+}
 
 ?>
