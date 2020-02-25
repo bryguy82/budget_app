bed45c331e00dd3367ed19201f5eaf5033150463
Anderson-Bryan <43304311+bryguy82@users.noreply.github.com>
2020-02-25 11:29:38 -0300

    Logout button added

--- common/nav.php
+++ common/nav.php
@@ -1,7 +1,22 @@
-<button onclick="toggleMenu()" class="menuButton">&#9776;</button>
-<ul id="clickMenu" class="hide">
-    <li><a href='/budget/' title='Budget home page'>Home</a></li>
-    <li><a href='/budget/view/manage_save.php' title='Budget saving page'>Track Saving</a></li>
-    <li><a href='/budget/view/manage_spend.php' title='Budget spending page'>Track Spending</a></li>
-    <li><a href='/budget/view/manage_debt.php' title='Budget debt page'>Track Debt</a></li>
-</ul>
+<div class="nav">
+    <button onclick="toggleMenu()" class="menuButton">&#9776;</button>
+    <ul id="clickMenu" class="hide">
+        <li><a href="/budget/" title="Budget home page">Home</a></li>
+        <li><a href="/budget/saving/?action=save" title="Budget saving page">Track Saving</a></li>
+        <li><a href="/budget/spending/?action=spend" title="Budget spending page">Track Spending</a></li>
+        <li><a href="/budget/debt/?action=debt" title="Budget debt page">Track Debt</a></li>
+
+        <?php
+        if(isset($_SESSION['loggedin'])) {
+        echo 
+            '<li class="logout">
+                <a href="/budget/accounts/?action=Logout">
+                    <img alt="logout icon" src="/budget/images/logout-icon_75.png">
+                </a>
+            </li>';
+        }
+
+        ?>
+
+    </ul>
+</div>
